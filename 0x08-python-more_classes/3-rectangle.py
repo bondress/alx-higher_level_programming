b5f4cab0914e697f970a924a11a6707a4300d7a1
Laide Endeley <endeleylaide@gmail.com>
2023-08-07 07:13:02 -0700

    Updated 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -24,7 +24,6 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    """Sets width attribute"""
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -39,7 +38,6 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Sets height attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -58,9 +56,9 @@ class Rectangle:
 
     def __str__(self):
         """
-        Return printable representation of rectangle
+        Return printable representation of Rectangle.
 
-        Shows rectangle with the character #
+        Print rectangle with # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
